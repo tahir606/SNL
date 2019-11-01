ace3eafd9e75d0da204a3aa2a5c207125fbf7bef
Tahir Shakir <tahir60652@gmail.com>
2019-11-01 19:40:23 +0500

    2Days2Go

--- src/Game.java
+++ src/Game.java
@@ -1,69 +1,221 @@
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 //Controller Class
 public class Game {
 
-    private int playerPosition = 0,
+    public static int playerPosition = 0,
             computerPosition = 0;
+
+    public static int prevPlayerPosition = 0,
+            prevComputerPosition = 0;
+
+    public static boolean playerTurn = false,
+            computerTurn = false;
+
+    private boolean gameStart = true;
+
+    private int dice = 0;
 
     private UserInterface ui;
 
     public Game() {
         ui = new UserInterface();
-        ui.restartBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Start Game Again");
-                playerPosition = 0;
-                computerPosition = 0;
-                ui.setNoticeBoardTxt("To decide who plays first: Roll the dice");
-//                int p = waitForDiceRoll(); //THIS IS MAKING THE SHIT HANG CORRECT THIS
-                int c = Dice.rollDice();
-//                startGame();
-            }
-        });
+        setRestartBtn();
+        setRollDiceBtn();
         startGame();
-    }
-
-    private void computerTurn() {
-        int c = Dice.rollDice();
-        ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " Computer Rolls a " + c);
-        computerPosition = computerPosition + c;
     }
 
     public void startGame() {
         playerPosition = 0;
         computerPosition = 0;
         ui.setNoticeBoardTxt("To decide who plays first: Roll the dice");
-        int p = waitForDiceRoll();
-        int c = Dice.rollDice();
-        ui.setNoticeBoardTxt("You Rolled: " + p + " Computer Rolled: " + c);
-        //User rolled greater
-        if (UserInterface.diceRoll > c) {
-            ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " Your turn First! Please roll Dice");
-            playerTurn();
-        } else {
-            ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " Computer Plays First!");
-            computerTurn();
-        }
+        gameStart = true;
+    }
+
+    public void setRestartBtn() {
+        ui.restartBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startGame();
+            }
+        });
+    }
+
+    public void setRollDiceBtn() {
+        ui.rollDiceBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dice = Dice.rollDice();
+                //Deciding who plays first
+                if (gameStart) {
+                    int c = Dice.rollDice();
+                    ui.setNoticeBoardTxt("You Rolled a: " + dice + " The Computer rolled a: " + c);
+                    gameStart = false;
+                    if (dice > c) {
+                        ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " You Play First! Roll The Dice");
+                        playerTurn = true;
+                    } else {
+                        ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " Computer Plays First! Roll The Dice");
+                        computerTurn = true;
+                    }
+                } else {
+                    if (playerTurn) {
+                        playerTurn();
+                    } else if (computerTurn) {
+                        computerTurn();
+                        removeLabel(2);
+                    }
+                }
+            }
+        });
     }
 
     private void playerTurn() {
-        int p = waitForDiceRoll();
-        ui.setNoticeBoardTxt(" You Rolled a " + p);
-        computerPosition = computerPosition + p;
+        prevPlayerPosition = playerPosition;
+        playerPosition = playerPosition + dice;
+        ui.setNoticeBoardTxt("Please Move " + dice + " Paces! Computer's turn! Roll The Dice!");
+        playerTurn = false;
+        computerTurn = true;
+//        if (checkForSnakesAndLadders(1))
+//            movePlayer();
+//        checkForWin(1);
+//        System.out.println("PP: " + playerPosition);
     }
 
-    private int waitForDiceRoll() {
-        UserInterface.diceClick = false;
-        while (!UserInterface.diceClick) {
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
+    private void computerTurn() {
+        prevComputerPosition = computerPosition;
+        computerPosition = computerPosition + dice;
+        ui.setNoticeBoardTxt(ui.getNoticeBoardTxt() + " \n Computer Rolled " + dice + " Paces " + " Your Turn Now!");
+//        checkForSnakesAndLadders(2);
+        moveComputer();
+        computerTurn = false;
+        playerTurn = true;
+//        checkForWin(2);
+    }
+
+//    private boolean checkForSnakesAndLadders(int i) {
+//        int score;
+//        if (i == 1)
+//            score = playerPosition;
+//        else
+//            score = computerPosition;
+//
+//        System.out.println("Score: " + score);
+//
+//        for (int j = 0; j < 2; j++) {
+//            //If at ladder start
+//            if (score == UserInterface.ladderStarts[j]) {
+//                score = UserInterface.ladderEnds[j];
+//                break;
+//            }
+//
+//            if (score == UserInterface.snakeStarts[j]) {
+//                score = UserInterface.snakeEnds[j];
+//                break;
+//            }
+//        }
+//
+//        System.out.println("Before : " + score);
+//        if (i == 1)
+//            playerPosition = score;
+//        else
+//            computerPosition = score;
+//        System.out.println("After : " + playerPosition);
+//        if (playerPosition < score || playerPosition > score) {
+//            System.out.println("After 2: " + score);
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//
+//    private void checkForWin(int i) {
+//        int score;
+//        if (i == 1) {
+//            score = playerPosition;
+//        } else {
+//            score = computerPosition;
+//        }
+//
+//        if (score == 30) {
+//            if (i == 1)
+//                ui.setNoticeBoardTxt("You Win!");
+//            else
+//                ui.setNoticeBoardTxt("Computer Wins!");
+//        } else {
+//            if (i == 1)
+//                playerPosition = (prevPlayerPosition != 0 ? prevPlayerPosition : playerPosition);
+//            else
+//                computerPosition = (prevComputerPosition != 0 ? prevComputerPosition : computerPosition);
+//        }
+//    }
+
+    private void moveComputer() {
+        Component[] components = ui.boardPanel.getComponents();
+        int index = 0;
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] instanceof JButton) {
+                JButton btn = (JButton) components[i];
+                if (((Integer) btn.getClientProperty("id")) == computerPosition) {
+                    index = i;
+                    break;
+                }
             }
         }
-        return UserInterface.diceRoll;
+        ((JButton) ui.boardPanel.getComponent(index)).setText(((JButton) ui.boardPanel.getComponent(index)).getText() + " C ");
     }
+
+    private void movePlayer() {
+        Component[] components = ui.boardPanel.getComponents();
+        int index = 0;
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] instanceof JButton) {
+                JButton btn = (JButton) components[i];
+                if (((Integer) btn.getClientProperty("id")) == playerPosition) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        ((JButton) ui.boardPanel.getComponent(index)).setText(((JButton) ui.boardPanel.getComponent(index)).getText() + " Y ");
+    }
+
+    private void removeLabel(int p) {
+        Component[] components = ui.boardPanel.getComponents();
+
+        if (p == 1) {
+            int index = -1;
+            for (int i = 0; i < components.length; i++) {
+                if (components[i] instanceof JButton) {
+                    JButton btn = (JButton) components[i];
+                    if (((Integer) btn.getClientProperty("id")) == prevPlayerPosition) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if (index > -1) {
+                ((JButton) ui.boardPanel.getComponent(index)).setText(((JButton) ui.boardPanel.getComponent(index)).getText().replace("Y", ""));
+            }
+        } else if (p == 2) {
+            int index = -1;
+            for (int i = 0; i < components.length; i++) {
+                if (components[i] instanceof JButton) {
+                    JButton btn = (JButton) components[i];
+                    if (((Integer) btn.getClientProperty("id")) == prevComputerPosition) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if (index > -1) {
+                ((JButton) ui.boardPanel.getComponent(index)).setText(((JButton) ui.boardPanel.getComponent(index)).getText().replace("C", ""));
+            }
+        }
+    }
+
 
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -6,15 +6,21 @@ import java.awt.event.ActionListener;
 //A Boundary Class which deals only with the User Interface
 public class UserInterface {
 
-    public static int diceRoll = 0;
-    public static boolean diceClick = false, restartClick = false;
+    public volatile static boolean paceClick = false;
 
     private JFrame frame = new JFrame();
+    public JPanel boardPanel;
 
     private JLabel noticeBoardLbl = new JLabel("Notice Board");
     private JLabel title = new JLabel("Snakes And Ladders");
     public JButton restartBtn = new JButton("Restart");
-    private JButton rollDiceBtn = new JButton("Roll Dice");
+    public JButton rollDiceBtn = new JButton("Roll Dice");
+
+    public static final int[] ladderStarts = {4, 12};
+    public static final int[] ladderEnds = {14, 22};
+
+    public static final int[] snakeStarts = {20, 16};
+    public static final int[] snakeEnds = {7, 5};
 
     public UserInterface() {
         createScreen();
@@ -29,30 +35,13 @@ public class UserInterface {
         //Center
         JPanel board = createBoard();
 
-        //Roll Dice Btn
-        rollDiceBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                diceRoll = Dice.rollDice();
-                diceClick = true;
-            }
-        });
-
-        //Restart Button
-        restartBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
         frame.add(title, BorderLayout.NORTH);
         frame.add(noticeBoardLbl, BorderLayout.SOUTH);
         frame.add(rollDiceBtn, BorderLayout.EAST);
         frame.add(restartBtn, BorderLayout.WEST);
         frame.add(board, BorderLayout.CENTER);
 
-        frame.setSize(600, 600);
+        frame.setSize(700, 700);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setVisible(true);
     }
@@ -61,8 +50,8 @@ public class UserInterface {
     int matrix[][] = new int[row][col];
 
     private JPanel createBoard() {
-        JPanel p = new JPanel();
-        p.setLayout(new GridLayout(row, col));
+        boardPanel = new JPanel();
+        boardPanel.setLayout(new GridLayout(row, col));
 
         int i = 0;
         for (int r = 0; r < row; r++) {
@@ -78,18 +67,16 @@ public class UserInterface {
                 i = 25;
             }
             for (int c = 0; c < col; c++) {
-                Tile tile = new Tile(r, c, matrix, i);
-                tile.setText(String.valueOf(i));
-                tile.putClientProperty("id", i);
+                Tile tile = new Tile(r, c, matrix, i, this);
                 if (r % 2 == 0) {
                     i++;
                 } else {
                     i--;
                 }
-                p.add(tile);
+                boardPanel.add(tile);
             }
         }
-        return p;
+        return boardPanel;
     }
 
     public void setNoticeBoardTxt(String text) {
@@ -100,6 +87,112 @@ public class UserInterface {
         return noticeBoardLbl.getText();
     }
 
+    public void removeLabel(int p) {
+        Component[] components = boardPanel.getComponents();
+
+        if (p == 1) {
+            int index = -1;
+            for (int i = 0; i < components.length; i++) {
+                if (components[i] instanceof JButton) {
+                    JButton btn = (JButton) components[i];
+                    if (((Integer) btn.getClientProperty("id")) == Game.prevPlayerPosition) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if (index > -1) {
+                ((JButton) boardPanel.getComponent(index)).setText(((JButton) boardPanel.getComponent(index)).getText().replace("Y", ""));
+            }
+        } else if (p == 2) {
+            int index = -1;
+            for (int i = 0; i < components.length; i++) {
+                if (components[i] instanceof JButton) {
+                    JButton btn = (JButton) components[i];
+                    if (((Integer) btn.getClientProperty("id")) == Game.prevComputerPosition) {
+                        index = i;
+                        break;
+                    }
+                }
+            }
+            if (index > -1) {
+                ((JButton) boardPanel.getComponent(index)).setText(((JButton) boardPanel.getComponent(index)).getText().replace("C", ""));
+            }
+        }
+    }
+
+    private boolean checkForSnakesAndLadders(int i) {
+        int score;
+        if (i == 1)
+            score = Game.playerPosition;
+        else
+            score = Game.computerPosition;
+
+        System.out.println("Score: " + score);
+
+        for (int j = 0; j < 2; j++) {
+            //If at ladder start
+            if (score == UserInterface.ladderStarts[j]) {
+                score = UserInterface.ladderEnds[j];
+                break;
+            }
+
+            if (score == UserInterface.snakeStarts[j]) {
+                score = UserInterface.snakeEnds[j];
+                break;
+            }
+        }
+
+        System.out.println("Before : " + score);
+        if (i == 1)
+            Game.playerPosition = score;
+        else
+            Game.computerPosition = score;
+        System.out.println("After : " + Game.playerPosition);
+        if (Game.playerPosition < score || Game.playerPosition > score) {
+            System.out.println("After 2: " + score);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void checkForWin(int i) {
+        int score;
+        if (i == 1) {
+            score = Game.playerPosition;
+        } else {
+            score = Game.computerPosition;
+        }
+
+        if (score == 30) {
+            if (i == 1)
+                setNoticeBoardTxt("You Win!");
+            else
+                setNoticeBoardTxt("Computer Wins!");
+        } else {
+            if (i == 1)
+                Game.playerPosition = (Game.prevPlayerPosition != 0 ? Game.prevPlayerPosition : Game.playerPosition);
+            else
+                Game.computerPosition = (Game.prevComputerPosition != 0 ? Game.prevComputerPosition : Game.computerPosition);
+        }
+    }
+
+    private void movePlayer() {
+        Component[] components = boardPanel.getComponents();
+        int index = 0;
+        for (int i = 0; i < components.length; i++) {
+            if (components[i] instanceof JButton) {
+                JButton btn = (JButton) components[i];
+                if (((Integer) btn.getClientProperty("id")) == Game.playerPosition) {
+                    index = i;
+                    break;
+                }
+            }
+        }
+        ((JButton) boardPanel.getComponent(index)).setText(((JButton) boardPanel.getComponent(index)).getText() + " Y ");
+    }
+
     public static class Tile extends JButton {
 
         private final int[][] fModel;
@@ -108,12 +201,41 @@ public class UserInterface {
 
         private final int no;
 
-        public Tile(final int x, final int y, final int[][] model, final int no) {
+        private final UserInterface UI;
+
+        public Tile(final int x, final int y, final int[][] model, final int no, UserInterface ui) {
             fX = x;
             fY = y;
             fModel = model;
-
             this.no = no;
+            UI = ui;
+
+            putClientProperty("id", no);
+
+            String lbl = String.valueOf(no);
+
+            for (int i = 0; i < 2; i++) {
+                //Ladder Starts
+                if (no == UI.ladderStarts[i]) {
+                    putClientProperty("Prop", "LS");    //Ladder Starts
+                    lbl = lbl + " LS ";
+                }
+                if (no == UI.ladderEnds[i]) {
+                    putClientProperty("Prop", "LE");    //Ladder Ends
+                    lbl = lbl + " LE ";
+                }
+                if (no == UI.snakeStarts[i]) {
+                    putClientProperty("Prop", "SS");    //Snake Starts
+                    lbl = lbl + " SS ";
+                }
+                if (no == UI.snakeEnds[i]) {
+                    putClientProperty("Prop", "SE");    //Snake Ends
+                    lbl = lbl + " SE ";
+                }
+            }
+
+            setText(lbl);
+
 
             addActionListener(new ActionListener() {
                 @Override
@@ -121,16 +243,23 @@ public class UserInterface {
                     Object property = getClientProperty("id");
                     if (property instanceof Integer) {
                         int objectCounter = ((Integer) property);
-                        // do stuff
-                        System.out.println("Property: " + objectCounter);
+                        System.out.println(Game.playerPosition);
+                        if (objectCounter == Game.playerPosition) {
+                            setText(getText() + " Y");
+                            UserInterface.paceClick = true;
+                            ui.removeLabel(1);
+                            if (UI.checkForSnakesAndLadders(1)) {
+
+                            }
+                            UI.checkForWin(1);
+                        } else {
+                            ui.setNoticeBoardTxt("Please Click the Correct Button");
+                        }
+
                     }
                 }
             });
         }
-
-//        private void updateNameFromModel() {
-//            setText(String.valueOf(fModel[fX][fY]));
-//        }
 
     }
 }
